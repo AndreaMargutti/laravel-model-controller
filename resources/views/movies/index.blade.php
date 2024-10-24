47a5d324d312fb25b3f9f5620844e7d147df4232
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-24 16:08:05 +0200

    completed movies.index page structure

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,13 +4,24 @@
 
     <h1>Ciao sono la pagina dei Movies</h1>
 
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             @forelse ($movie as $movie )
 
-                <div class="col-6">
+                <div class="col-4 mb-4">
                     <!--Inserisco la Card-->
-
+                    <div class="card w-100">
+                        <div class="card-body">
+                          <h5 class="card-title"> {{$movie->title}} </h5>
+                          <p class="card-text"> {{$movie->id}} </p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><strong>Titolo Originale:</strong> {{$movie->original_title}} </li>
+                        <li class="list-group-item"><strong>Nazionalità:</strong> {{$movie->nationality}} </li>
+                        <li class="list-group-item"><strong>Data d'Uscita:</strong> {{$movie->date}} </li>
+                        <li class="list-group-item"><strong>Voto:</strong> {{$movie->vote}} </li>
+                        </ul>
+                    </div>
                 </div>
 
             @empty
